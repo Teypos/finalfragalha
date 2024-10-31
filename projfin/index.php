fc564ea5fe15ad209d133f3587fe069027868810
Teypos <164430372+Teypos@users.noreply.github.com>
2024-10-31 13:17:26 +0000

    +fonts e bootstrap

--- projfin/index.php
+++ projfin/index.php
@@ -1,24 +1,68 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Frãgalhã</title>
 </head>
 <body>
 	<script>0</script>
 	
-	<div class="fore">
-	<h1>Frãgalhã</h1>
-	<a href="gamehub.php" target="_blank">Gamehub?</a>
+	
+
+	<nav class="navbar navbar-expand-lg bg-body-tertiary fore">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Frãgalhã</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">1ª Página</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropper
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+	
+	
+	<div class="fore" id="over">
+	<h1 id="over">Frãgalhã</h1>
+	<h3 id="over"><a href="gamehub.php" target="_blank">Gamehub?</a></h3>
 	</div>
 	<br>
-	<div class="fore">
+	<div class="fore" id="over">
 	<h3>ESTE SITE É BUÉ GAMING MALTA (+)</h3> <img src="https://cdn.britannica.com/86/2286-050-C9E3A660/Flag-Malta.jpg" style="height:20%; width:20%;">
 	<br>
-	WOSWOSWOSWOSWOSWOSWOSWOSWOSWOSWOS
+	<h3>WOSWOSWOSWOSWOSWOSWOSWOSWOSWOSWOS</h3>
 	<a href="https://fragalha.atwebpages.com" target="_blank"><img src="images/gamer.png" alt=":) you can't see me"class="center">
 	</div>
+	
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>   
